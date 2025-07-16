7bfcd0072b50d3e29ec80ca7649ea433136020d8
Ashington Munene <munenefrank11@gmail.com>
2025-07-16 20:11:57 +0300

    updates SpicyFoodList

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,23 +4,43 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
-  function handleAddFood() {
+  {/*function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+    console.log(newFoodArray);
+  }*/}
+  
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id}  onClick= {() =>handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+  const handleClick = (id) => {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+  function handleLiClick(id) {
+  const newFoodArray = foods.map((food) => {
+    if (food.id === id) {
+      return {
+        ...food,
+        heatLevel: food.heatLevel + 1,
+      };
+    } else {
+      return food;
+    }
+  });
+  setFoods(newFoodArray);
+}
 
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
+      <button onClick={handleClick}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
   );
-}
+};
 
 export default SpicyFoodList;
